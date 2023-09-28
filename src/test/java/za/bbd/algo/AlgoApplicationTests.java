4dea13c5b6dc7acc7e35365916afa85d2332b433
george_pauer <george.pauer1@vcontractor.co.za>
2023-09-29 00:06:34 +0200

    selenium stuff

--- src/test/java/za/bbd/algo/AlgoApplicationTests.java
+++ src/test/java/za/bbd/algo/AlgoApplicationTests.java
@@ -6,16 +6,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 class AlgoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+		driver.get("http://localhost:8080/");
+		Thread.sleep(2000);
+		WebElement upload = driver.findElement(By.id("upload"));
+		Thread.sleep(2000);
+		upload.sendKeys("C:\\Users\\paueg001\\Documents\\algostuff\\test.csv");
+		Thread.sleep(2000);
+		WebElement card = driver.findElement(By.className("card"));
+		card.click();
+		Thread.sleep(2000);
 		driver.quit();
 	}
 
