217381e473afcee6d2ffaf22d0d9285611086fe1
Hewitt Nyambalo <hewitt@bbd.co.za>
2023-09-28 19:59:59 +0200

    file upload

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,14 +1,48 @@
 
 let data = undefined
 
+
+function showResult() {
+  
+}
+
+async function getFileContent() {
+  var file = document.getElementById("upload").files[0];
+  console.log(file)
+  if (file) {
+    var reader = new FileReader();
+    reader.readAsText(file, "UTF-8");
+    await new Promise(resolve => reader.onload = () => resolve());
+    let content = reader.result
+    return content;
+  } else {
+    console.log('no such file');
+    throw new Error("no such file")
+  }
+}
+
 function algo_card(details) {
   let card = document.createElement('article');
   let title = document.createElement('strong');
   let desc = document.createElement('p');
 
   card.classList.add('card')
-  card.onclick = () => {
-    console.log('use: ', details.route)
+  
+  card.onclick = async () => {
+    const content = await getFileContent();
+    console.log(content)
+    
+    fetch('/sort', {
+      method: "POST",
+      body: {
+        algo: details.name, 
+        data: content
+      }
+    }).then(response => {
+      response.json(result => {
+        showResponse(result)
+      })
+    })
   }
 
   title.innerText = details.name;
